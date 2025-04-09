5f9c21d84445b33f0d43e2204991140040861e13
jasurbek1700 <jasurbekjaloli55@gmail.com>
2025-04-09 20:53:49 +0500

    yangi ozgarish

--- git/Program.cs
+++ git/Program.cs
@@ -1,2 +1,9 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Net.Security;
+
+class program
+{
+     public static void Main(string[] args)
+    {
+        Console.WriteLine("hello ");
+    }
+}
