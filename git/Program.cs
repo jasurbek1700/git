d1df1de2451e077ea955acd59a6811c36afa949b
jasurbek1700 <jasurbekjaloli55@gmail.com>
2025-04-09 21:20:28 +0500

    yangi funksya

--- git/Program.cs
+++ git/Program.cs
@@ -1,9 +1,74 @@
 ﻿using System.Net.Security;
 
-class program
+
+class Program
 {
-     public static void Main(string[] args)
+    public static void Main()
     {
+        //    Console.WriteLine(" produc shop ");
+        //    Console.Write("1-Mahsulotlar royxati | 2- Sotib olingan mahsulotlar | 3- Sotib olish  ");
+
+        //    int[] arrey_kodlar = { 1, 2, 3, 4 };
+
+        //    string[] arrey_mahsulotnomi = { "presipka", "shampun", "molochka", "krem" };
+
+        //    double[] arrey_narxlari = { 35.500000, 40.500, 65.300, 25.100 };
+
+        //    string[] arrey_savat = new string[4];
+        //    Console.WriteLine("\n Qaysi amalni bajarmoqchisiz ");
+
+        //    int number = Convert.ToInt32(Console.ReadLine());
+        //    Console.WriteLine();
+
+        //    for (int i = 0; i < arrey_kodlar.Length; i++)
+        //    {
+        //        if (number == 1)
+        //        {
+        //            Console.Write(arrey_kodlar[i] + "         ");
+        //            Console.Write(arrey_mahsulotnomi[i] + "        ");
+        //            Console.Write(arrey_narxlari[i] + "\n");
+        //        }
+        //    }
+
+        //    for (int i = 0; i < arrey_savat.Length; i++)
+        //    {
+        //        if (number == 2 && arrey_savat.Length != 0)
+        //        {
+        //            Console.Write(arrey_savat[i] + "   , ");
+
+
+        //        }
+        //    }
+
+
+        //    for (int i = 0; i < arrey_mahsulotnomi.Length; i++)
+        //    {
+
+
+        //        if (number == 3)
+        //        {
+
+        //            Console.Write(arrey_kodlar[i] + "         ");
+        //            Console.Write(arrey_mahsulotnomi[i] + "        ");
+        //            Console.Write(arrey_narxlari[i] + "\n");
+
+        //        }
+
+        //    }
+        //    Console.Write("\n Qaysi mahsulorni sotib olmoqchisiz");
+        //    int namber2 = Convert.ToInt32(Console.ReadLine());
+        //    if (namber2 == 1) { }
+
+
+        Console.WriteLine("hello ");
+        Console.WriteLine("hello ");
+        Console.WriteLine("hello ");
+        Console.WriteLine("hello ");
+        Console.WriteLine("hello ");
+        Console.WriteLine("hello ");
+        Console.WriteLine("hello ");
+        Console.WriteLine("hello ");
         Console.WriteLine("hello ");
     }
+
 }
